777d2d424c5e89a9488ea6201b4b768e979aca3d
Miheller Peter <peti.miheller@gmail.com>
2024-10-09 16:52:34 +0300

    First exercise done. Main is also implemented with the exempels from the homework

--- src/Ex1/UniversityGrades.java
+++ src/Ex1/UniversityGrades.java
@@ -25,10 +25,41 @@ public class UniversityGrades {
         for (int grade : grades) {
             sum += grade;
         }
-        double average = sum / grades.length;
-        return average;
+        return sum / grades.length;
     }
 
+    // 3.
+    public int[] getRoundedGrades(int[] grades) {
+        int[] roundedGrades = new int[grades.length];
+        for (int i = 0; i < grades.length; i++) {
+            roundedGrades[i] = roundGrade(grades[i]);
+        }
+        return roundedGrades;
+    }
+
+
+    private int roundGrade(int grade) {
+        if (grade < 38) {
+            return grade;
+        }
+        int nextMultipleOfFive = ((grade / 5) + 1) * 5;
+        if (nextMultipleOfFive - grade < 3) {
+            return nextMultipleOfFive;
+        }
+        return grade;
+    }
+
+    // 4.
+    public int getMaxRoundedGrade(int[] grades) {
+        int maxRoundedGrade = 0;
+        for (int grade : grades) {
+            int roundedGrade = roundGrade(grade);
+            if (roundedGrade > maxRoundedGrade) {
+                maxRoundedGrade = roundedGrade;
+            }
+        }
+        return maxRoundedGrade;
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,24 @@ import Ex1.UniversityGrades;
 public class Main {
     public static void main(String[] args) {
         UniversityGrades grades = new UniversityGrades();
-        int[] gradesArray = {84, 29, 57, 33, 89, 73, 38};
+        int[] gradesArray = {29, 37, 38, 41, 84, 67};
+        //1
         int[] failingGrades = grades.getFailingGrades(gradesArray);
         System.out.println("1. Nicht ausreichende Noten: ");
         for (int grade : failingGrades) {
             System.out.print(grade + "; ");
         }
+        //2
+        double averageGrade = grades.getAverageGrade(gradesArray);
+        System.out.println("\n2. Durchschnitt der Noten: " + averageGrade);
+        //3
+        int[] roundedGrades = grades.getRoundedGrades(gradesArray);
+        System.out.println("3. Gerundete Noten: ");
+        for (int grade : roundedGrades) {
+            System.out.print(grade + "; ");
+        }
+        //4
+        int maxRoundedGrade = grades.getMaxRoundedGrade(gradesArray);
+        System.out.println("\n4. Die maximale abgerundete Note: " + maxRoundedGrade);
     }
 }
