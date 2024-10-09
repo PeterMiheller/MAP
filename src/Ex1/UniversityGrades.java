47b17fb71b072a66ea409b6076873b37211a3c04
Miheller Peter <peti.miheller@gmail.com>
2024-10-09 16:25:32 +0300

    From the first exercise 1 and 2 are done. In the main it is only the 1 subpoint implemented

---
+++ src/Ex1/UniversityGrades.java
@@ -0,0 +1,34 @@
+package Ex1;
+
+public class UniversityGrades {
+    //1.
+    public int[] getFailingGrades(int[] grades) {
+        int count = 0;
+        for (int grade : grades) {
+            if (grade < 40) {
+                count++;
+            }
+        }
+        int[] failingGrades = new int[count];
+        int index = 0;
+        for (int grade : grades) {
+            if (grade < 40) {
+                failingGrades[index] = grade;
+                index++;
+            }
+        }
+        return failingGrades;
+    }
+    //2.
+    public double getAverageGrade(int[] grades) {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        double average = sum / grades.length;
+        return average;
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import Ex1.UniversityGrades;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        UniversityGrades grades = new UniversityGrades();
+        int[] gradesArray = {84, 29, 57, 33, 89, 73, 38};
+        int[] failingGrades = grades.getFailingGrades(gradesArray);
+        System.out.println("1. Nicht ausreichende Noten: ");
+        for (int grade : failingGrades) {
+            System.out.print(grade + "; ");
         }
     }
 }
