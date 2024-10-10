5e44512a0d1dc8f4d269d09d4c08630f1f500573
Miheller Peter <peti.miheller@gmail.com>
2024-10-10 16:56:25 +0300

    Created a Human class to store the buget of Markus. Implementes the third subpoint.

--- src/Ex4/ElektronikShop.java
+++ src/Ex4/ElektronikShop.java
@@ -4,6 +4,7 @@ import javax.sound.midi.Instrument;
 import java.util.List;
 
 public class ElektronikShop {
+    //1
     public double findCheapestKeyboard(List<Keyboard> keyboards){
         Keyboard cheapest = keyboards.getFirst();
         double minPrice = cheapest.price;
@@ -17,6 +18,7 @@ public class ElektronikShop {
         return cheapest.price;
     }
 
+    //2
     public double findExpensiveInstrument(List<ElectricInstrument> device){
         ElectricInstrument expensive = device.getFirst();
         double maxPrice = expensive.price;
@@ -29,5 +31,18 @@ public class ElektronikShop {
         }
         return expensive.price;
     }
+
+    //3
+    public double USBMarkusCanBuy(Human Markus,List<USB> usbDevice){
+        ElectricInstrument expensive_USB= new USB(usbDevice.getFirst().price);
+        for(USB device: usbDevice){
+            if(device.price > expensive_USB.price && expensive_USB.price<= Markus.buget){
+                expensive_USB.price=device.price;
+            }
+        }
+        return expensive_USB.price;
+    }
+
+
 
 }
---
+++ src/Ex4/Human.java
@@ -0,0 +1,9 @@
+package Ex4;
+
+public class Human {
+    double buget;
+
+    public Human(double buget) {
+        this.buget = buget;
+    }
+}
